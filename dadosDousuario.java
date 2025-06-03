6d3e92ff1351109b67e4760f3dd3a1ba01e83e35
PauloHCabral <PH paulohenriquecabral@outlook.com>
2025-06-03 01:10:29 -0300

    Primeiro commit: adiciona dadosDousuario.java

---
+++ dadosDousuario.java
@@ -0,0 +1,19 @@
+/**
+ * @author Paulo Henrique
+ */
+
+import java.util.Scanner;
+public class dadosDousuario {
+    public static void main(String[] args) {
+
+        Scanner inseri_nome = new Scanner(System.in);
+        System.out.println("Olá mundo !");
+
+        System.out.println("Digite seu :");
+
+        String nomeDousuario = inseri_nome.nextLine();
+        System.out.println("Seja bem-vindo "+nomeDousuario);
+
+
+    }
+}
