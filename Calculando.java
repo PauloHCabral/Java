04f31c549c57d08c64ef4ab78fc2a16d25f159df
PauloHCabral <PHpaulohenriquecabral@outlook.com>
2025-06-04 00:20:23 -0300

    Somando valores

---
+++ Calculando.java
@@ -0,0 +1,26 @@
+/**
+ * @author Paulo Henrique
+ */
+
+// Classe chamada calculando
+public class Calculando {
+    public static void main(String [] args){
+        // Apresentando na tela, o que o programa fará
+        System.out.println("Calculando valores");
+
+        // Declarando duas variáveis a e b, do tipo inteiro, que receberá dois valores
+        int a = 5;
+        int b = 5;
+
+        // Realizando a soma dos dois valores, e a soma desses valores, ficará armazenada dentro da variável soma
+        int soma = a  + b;
+
+        // Exibindo o resultado da soma
+        System.out.println("A soma dos valores a + b é = " +soma);
+
+
+
+
+    }
+
+}
